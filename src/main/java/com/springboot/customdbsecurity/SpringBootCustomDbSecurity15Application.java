f63d46cd825275f0c42815f8d533a5eab523db0a
Saikumar Bandigi <bandigisaikumar@gmail.com>
2023-12-30 19:45:05 +0530

    Initial commit

---
+++ src/main/java/com/springboot/customdbsecurity/SpringBootCustomDbSecurity15Application.java
@@ -0,0 +1,20 @@
+package com.springboot.customdbsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.springboot.customdbsecurity.Repository.UsersRepository;
+
+// Topic: Dynamic Authentication and Authorization
+@SpringBootApplication
+public class SpringBootCustomDbSecurity15Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCustomDbSecurity15Application.class, args);
+    }
+
+
+}
